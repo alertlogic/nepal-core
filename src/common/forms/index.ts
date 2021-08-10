abdeed7b7f41837d024b184f1bd61e3da879da61
cristhian-fuertes <cristhian.e.fuertes@gmail.com>
2021-08-10 11:42:27 -0500

    Rolling back forms refactor

--- src/common/forms/index.ts
+++ src/common/forms/index.ts
@@ -1,18 +1,1 @@
-/**
- * Please see README before adding properties here :)
- */
-
-export interface AlDynamicFormControlElement<Type=any> {
-    type: string;
-    property: string;
-    label?: string;
-    secret?: boolean;
-    description?: string;
-    value?: Type;
-    defaultValue?: Type;
-    optional?: boolean;
-    options?: {
-        label: string;
-        value: Type;
-    }[];
-}
+export * from "./types";
